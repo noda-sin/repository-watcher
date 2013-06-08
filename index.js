da07dad02e9e74222175569ac60a9d3500d5ec04
Noda Shimpei <noda.sin@gmail.com>
2013-06-09 04:40:15 +0900

    Implemented to loop watching repository

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var url      = require('url')
   , async    = require('async')
   , request  = require('request')
   , argv     = require('optimist').default({
-    token: process.env.GITHUB_TOKEN,
-    proxy: process.env.http_proxy,
-    org:   process.env.GITHUB_ORG
+    token:    process.env.GITHUB_TOKEN,
+    proxy:    process.env.http_proxy,
+    org:      process.env.GITHUB_ORG,
+    interval: 1000 * 60
   }).argv;
 
 if (!argv.org) {
@@ -16,6 +17,26 @@ if (!argv.token) {
   console.error('Require GitHub Personal API Token.');
   process.exit(1);
 }
+
+setInterval(function() {
+  requestToGithub('/orgs/' + argv.org + '/members', function(err, members) {
+    if (err) {
+      printError(err);
+      return;
+    }
+    members.forEach(function(member) {
+      requestToGithub('/users/' + member.login + '/repos', function(err, repos) {
+        if (err) {
+          printError(err);
+          return;
+        }
+        repos.forEach(function(repo) {
+          console.log('repository name: ' + repo.full_name);
+        });
+      });
+    });
+  });
+}, argv.interval);
 
 function requestToGithub(path, callback) {
   var option = {
@@ -33,7 +54,6 @@ function requestToGithub(path, callback) {
   if (argv.proxy) {
     option.proxy = argv.proxy;
   }
-
   request.get(option, function(err, res, json) {
     if (err) {
       callback(err);
@@ -43,34 +63,6 @@ function requestToGithub(path, callback) {
   });
 }
 
-async.waterfall([
-  function(callback) {
-    requestToGithub('/orgs/' + argv.org + '/members', function(err, members) {
-      if (err) {
-        callback(err);
-        return;
-      }
-      members.forEach(function(member) {
-        callback(null, member);
-      });
-    });
-  },
-  function(member, callback) {
-    requestToGithub('/users/' + member.login + '/repos', function(err, repos) {
-      if (err) {
-        callback(err);
-        return;
-      }
-      repos.forEach(function(repo) {
-        callback(null, repo);
-      });
-    });
-  }
-  ], function(err, repo) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('repository name: ' + repo.full_name);
-  }
-);
+function printError(error) {
+  console.error('[' + new Date().toUTCString() + '] ' + err);
+}
